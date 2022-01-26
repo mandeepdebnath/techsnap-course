ac89bcc41ec84a7f12a1dcc7801e2c336c719f14
Mandeep Debnath <mandeepdebnath@gmail.com>
2022-01-26 11:30:22 +0530

    adding more lessons

--- main.js
+++ main.js
@@ -17,14 +17,14 @@ sectionBtn1.onclick = () => {
   console.log("heyy");
   subMenu1.classList.toggle("show");
 };
-// sectionBtn2.onclick = () => {
-//   console.log("heyy");
-//   subMenu2.classList.toggle("show");
-// };
-// sectionBtn3.onclick = () => {
-//   console.log("heyy");
-//   subMenu3.classList.toggle("show");
-// };
+sectionBtn2.onclick = () => {
+  console.log("heyy");
+  subMenu2.classList.toggle("show");
+};
+sectionBtn3.onclick = () => {
+  console.log("heyy");
+  subMenu3.classList.toggle("show");
+};
 
 let mobileNavBtn = document.querySelector(".mobile-nav-icon");
 let courseNav = document.querySelector(".sidebar-nav-container");
