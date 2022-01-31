ec1b4dae5d4c9539ab1fcbdefd8e707b94f30218
Mandeep Debnath <mandeepdebnath@gmail.com>
2022-01-31 20:34:06 +0530

    making sidebar header sticky

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let sectionBtn1 = document.querySelector(".section-btn-1");
 let sectionBtn2 = document.querySelector(".section-btn-2");
-let sectionBtn3 = document.querySelector(".section-btn-3");
-let sectionBtn4 = document.querySelector(".section-btn-4");
-let sectionBtn5 = document.querySelector(".section-btn-5");
+// let sectionBtn3 = document.querySelector(".section-btn-3");
+// let sectionBtn4 = document.querySelector(".section-btn-4");
+// let sectionBtn5 = document.querySelector(".section-btn-5");
 
 // let sectionBtn6 = document.querySelector(".section-btn-6");
 // let sectionBtn7 = document.querySelector(".section-btn-7");
@@ -12,9 +12,9 @@ let sectionBtn5 = document.querySelector(".section-btn-5");
 
 let subMenu1 = document.querySelector(".sub-menu-1");
 let subMenu2 = document.querySelector(".sub-menu-2");
-let subMenu3 = document.querySelector(".sub-menu-3");
-let subMenu4 = document.querySelector(".sub-menu-4");
-let subMenu5 = document.querySelector(".sub-menu-5");
+// let subMenu3 = document.querySelector(".sub-menu-3");
+// let subMenu4 = document.querySelector(".sub-menu-4");
+// let subMenu5 = document.querySelector(".sub-menu-5");
 // let subMenu6 = document.querySelector(".sub-menu-6");
 // let subMenu7 = document.querySelector(".sub-menu-7");
 // let subMenu8 = document.querySelector(".sub-menu-8");
@@ -37,18 +37,18 @@ sectionBtn2.onclick = () => {
   console.log("heyy");
   subMenu2.classList.toggle("show");
 };
-sectionBtn3.onclick = () => {
-  console.log("heyy");
-  subMenu3.classList.toggle("show");
-};
-sectionBtn4.onclick = () => {
-  console.log("heyy");
-  subMenu4.classList.toggle("show");
-};
-sectionBtn5.onclick = () => {
-  console.log("heyy");
-  subMenu5.classList.toggle("show");
-};
+// sectionBtn3.onclick = () => {
+//   console.log("heyy");
+//   subMenu3.classList.toggle("show");
+// };
+// sectionBtn4.onclick = () => {
+//   console.log("heyy");
+//   subMenu4.classList.toggle("show");
+// };
+// sectionBtn5.onclick = () => {
+//   console.log("heyy");
+//   subMenu5.classList.toggle("show");
+// };
 // sectionBt6.onclick = () => {
 //   console.log("heyy");
 //   subMen6.classList.toggle("show");
@@ -198,3 +198,14 @@ let courseNav = document.querySelector(".sidebar-nav-container");
 mobileNavBtn.onclick = () => {
   courseNav.classList.toggle("show-mobile");
 };
+
+// document
+//   .querySelector(".sidebar-nav-container")
+//   .addEventListener("touchmove", preventScroll, { passive: false });
+
+// function preventScroll(e) {
+//   e.preventDefault();
+//   e.stopPropagation();
+
+//   return false;
+// }
